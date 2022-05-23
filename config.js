c938f90f01eda2aea4e806a45bb7218b71019868
Maxime Moreillon <m.moreillon@gmail.com>
2022-05-23 15:35:39 +0900

    prepared for comibination with mongoose version

--- config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv')
-
-// Parse environment variables
-dotenv.config()
-
-exports.uploads_directory_path = process.env.UPLOAD_DIRECTORY || "/usr/share/pv"
-exports.mongodb_export_file_name = 'mongodb_data.json'
