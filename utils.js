aa75df22fae5400daf360a943e1ab7ce4e3a278d
Maxime Moreillon <m.moreillon@gmail.com>
2022-06-01 08:52:13 +0900

    minor cleanup

--- utils.js
+++ utils.js
@@ -1,14 +1,6 @@
 const rimraf = require('rimraf')
 const fs = require('fs')
 
-exports.error_handling = (error, res) => {
-  console.log(error)
-  let status_code = error.code || 500
-  if(status_code === 11000) return res.status(400).send('Image already exists')
-  const message = error.message || error
-  res.status(status_code).send(message)
-}
-
 exports.remove_file = (file_path) => {
   return new Promise((resolve, reject) => {
     rimraf(file_path, (error) => {
