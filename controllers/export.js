ecbf9f248a0c3866d557dff9aa0a852ccdd1e58c
Maxime Moreillon <m.moreillon@gmail.com>
2022-08-11 09:14:53 +0900

    now using archiver for export

--- controllers/export.js
+++ controllers/export.js
@@ -1,4 +1,4 @@
-const AdmZip = require('adm-zip')
+const archiver = require('archiver');
 const Image = require('../models/image.js')
 const path = require('path')
 const fs = require('fs')
@@ -66,30 +66,65 @@ exports.export_images = async (req, res, next) => {
     const excel_file_path = path.join(__dirname, `../${uploads_directory}`, 'mongodb_data.xlsx')
     const temp_zip_path = path.join(__dirname, `../image_storage_service_export.zip`)
 
+    
+
     const images = await Image.find({})
     const images_json = images.map(i => i.toJSON())
 
     generate_excel(images_json, excel_file_path)
     generate_json(images_json, json_file_path)
 
-    const zip = new AdmZip()
+    const output = fs.createWriteStream(temp_zip_path);
+    const archive = archiver('zip', {
+      zlib: { level: 9 } // Sets the compression level.
+    });
 
-    zip.addLocalFolder(folder_to_zip)
-    zip.writeZip(temp_zip_path)
+    output.on('close', async () => {
+      console.log(`[Export] ${archive.pointer()} total bytes`);
+      console.log('[Export] archiver has been finalized and the output file descriptor has closed.');
+      res.download(temp_zip_path)
 
-    res.download(temp_zip_path)
+
+      // Cleanup of generated files
+      await delete_file(excel_file_path)
+      await delete_file(json_file_path)
+      await delete_file(temp_zip_path)
+
+      console.log(`[Export] Images exported`)
 
 
-    // Cleanup of generated files
-    await delete_file(excel_file_path)
-    await delete_file(json_file_path)
-    await delete_file(temp_zip_path)
+    });
 
-    // res.setHeader("Content-Type", "application/zip" )
-    // res.setHeader("Content-Disposition", `attachment; filename=${zip_filename}` )
-    // res.send(zipFileContents)
+    // This event is fired when the data source is drained no matter what was the data source.
+    // It is not part of this library but rather from the NodeJS Stream API.
+    // @see: https://nodejs.org/api/stream.html#stream_event_end
+    output.on('end', function () {
+      console.log('[Export] Data has been drained');
+    });
 
-    console.log(`[Export] Images exported`)
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on('warning', function (err) {
+      if (err.code === 'ENOENT') {
+        // log warning
+      } else {
+        // throw error
+        throw err;
+      }
+    });
+
+    // good practice to catch this error explicitly
+    archive.on('error', function (err) {
+      throw err;
+    });
+
+    console.log(`[Export] Exporting started`)
+
+    archive.pipe(output);
+    archive.directory(folder_to_zip, false);
+    archive.finalize();
+
+    
+
   }
   catch (error) {
     next(error)
