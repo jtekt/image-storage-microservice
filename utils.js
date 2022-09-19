caac93d27e44a49b2fe8c9338fd1d4a307e53290
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-20 07:36:59 +0900

    minor

--- utils.js
+++ utils.js
@@ -1,22 +1,24 @@
 const rimraf = require('rimraf')
 const fs = require('fs')
 
-exports.remove_file = (file_path) => {
-  return new Promise((resolve, reject) => {
-    rimraf(file_path, (error) => {
-      if(error) return reject(error)
-      resolve()
-    })
+exports.remove_file = (file_path) => new Promise((resolve, reject) => {
+  rimraf(file_path, (error) => {
+    if(error) return reject(error)
+    resolve()
   })
-}
+})
+
 
 exports.create_directory_if_not_exists = (target) => {
   let stat = null
+  
   try {
     stat = fs.statSync(target)
-  } catch (err) {
+  } 
+  catch (err) {
     fs.mkdirSync(target, { recursive: true })
   }
+
   if (stat && !stat.isDirectory()) {
     throw new Error(`Directory cannot be created because an inode of a different type exists at ${target}`);
   }
@@ -25,6 +27,7 @@ exports.create_directory_if_not_exists = (target) => {
 exports.compute_filters = (req) => {
 
   let filter = {}
+
   if(req.query.filter) {
     try {
       filter = JSON.parse(req.query.filter)
