9ca23d39dc1346d0e805bd3a49718067a44cf4cc
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-20 07:24:15 +0900

    minor

--- models/image.js
+++ models/image.js
@@ -1,12 +1,9 @@
 const { Schema, model } = require('mongoose')
 
 const options = { minimize: false }
+
 const imageSchema = new Schema({
-  file: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  file: { ype: String, required: true, unique: true },
   time: {type: Date, default: Date.now},
   data: {type: Schema.Types.Mixed, default: {}},
 }, options)
