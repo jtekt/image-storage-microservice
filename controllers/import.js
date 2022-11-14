b15c66a135ef23bc18a1a534e5094e333bfd3767
Maxime Moreillon <m.moreillon@gmail.com>
2022-11-15 07:56:26 +0900

    unzip first, load metadata after

--- controllers/import.js
+++ controllers/import.js
@@ -36,7 +36,10 @@ exports.import_images = async (req, res, next) => {
     const archive_path = path.join(__dirname, `../${archive_relative_path}`) 
 
     const directory = await unzipper.Open.file(archive_path)
+    
+    // Unzip the archive to the uploads directory
     const unzip_directory = path.join(__dirname, `../${uploads_directory}`)
+    await directory.extract({ path: unzip_directory })
 
     // Check if the archive contains the .json file containing the MonggoDB backup
     const contains_json = directory.files.some(({ path }) => path === mongodb_export_file_name )
@@ -53,8 +56,7 @@ exports.import_images = async (req, res, next) => {
       await mongodb_data_import(mongodb_data)
     }
 
-    // Unzip the archive to the uploads directory
-    await directory.extract({ path: unzip_directory })
+    
 
     // Remove the archive when done extracting
     await remove_file(archive_path)
