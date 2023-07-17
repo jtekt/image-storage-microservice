1b6b20e1f54a19c67e993823fc4e313428e4c53d
Maxime Moreillon <m.moreillon@gmail.com>
2023-07-17 14:33:53 +0900

    index on time

--- models/image.ts
+++ models/image.ts
@@ -1,14 +1,19 @@
-import { Schema, model } from 'mongoose'
-import IImage from '../interfaces/IImage'
+import { Schema, model } from "mongoose"
+import IImage from "../interfaces/IImage"
 
 const options = { minimize: false }
 
-const imageSchema = new Schema<IImage>({
-  file: { type: String, required: true, unique: true },
-  time: { type: Date, default: Date.now },
-  data: { type: Schema.Types.Mixed, default: {} },
-}, options)
+const imageSchema = new Schema<IImage>(
+  {
+    file: { type: String, required: true, unique: true },
+    time: { type: Date, default: Date.now },
+    data: { type: Schema.Types.Mixed, default: {} },
+  },
+  options
+)
 
-const Image = model('Image', imageSchema)
+imageSchema.index({ time: 1 })
+
+const Image = model("Image", imageSchema)
 
 export default Image
