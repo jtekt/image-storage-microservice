3854edb93f20ff31d5997eef6c24dcea5e7a2e60
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-12 11:40:36 +0900

    select

--- controllers/images.ts
+++ controllers/images.ts
@@ -1,139 +1,160 @@
-import Image from "../models/image"
-import path from "path"
-import createHttpError from "http-errors"
-import { directories } from "../config"
-import { remove_file, parse_query } from "../utils"
-import { Request, Response } from "express"
+import Image from '../models/image'
+import path from 'path'
+import createHttpError from 'http-errors'
+import { directories } from '../config'
+import { remove_file, parse_query } from '../utils'
+import { Request, Response } from 'express'
 
 interface NewImage {
-  _id?: string
-  time: Date
-  file: string
-  data: any
+    _id?: string
+    time: Date
+    file: string
+    data: any
 }
 
 export const upload_image = async (req: Request, res: Response) => {
-  // NOTE: Req.body is multipart form-data
-  if (!req.file) throw createHttpError(400, "File not provided")
+    // NOTE: Req.body is multipart form-data
+    if (!req.file) throw createHttpError(400, 'File not provided')
 
-  // TODO: Only allow images
-  const {
-    file: { originalname },
-    body,
-  } = req
+    // TODO: Only allow images
+    const {
+        file: { originalname },
+        body,
+    } = req
 
-  const { _id, time, file: filePath, json, data: bodyData, ...bodyRest } = body
+    const {
+        _id,
+        time,
+        file: filePath,
+        json,
+        data: bodyData,
+        ...bodyRest
+    } = body
 
-  const jsonData = json || bodyData
-  const data = jsonData ? JSON.parse(jsonData) : bodyRest
+    const jsonData = json || bodyData
+    const data = jsonData ? JSON.parse(jsonData) : bodyRest
 
-  const file = filePath || originalname
+    const file = filePath || originalname
 
-  const query = { file }
-  const itemProperties: NewImage = {
-    _id,
-    time: time ? new Date(time) : new Date(),
-    file,
-    data,
-  }
+    const query = { file }
+    const itemProperties: NewImage = {
+        _id,
+        time: time ? new Date(time) : new Date(),
+        file,
+        data,
+    }
 
-  const options = { upsert: true, new: true }
-  const newImage = await Image.findOneAndUpdate(query, itemProperties, options)
-  res.send(newImage)
+    const options = { upsert: true, new: true }
+    const newImage = await Image.findOneAndUpdate(
+        query,
+        itemProperties,
+        options
+    )
+    res.send(newImage)
 }
 
 export const read_images = async (req: Request, res: Response) => {
-  // Limiting here because parse_query also used in export
-  const { query, sort, order, limit = 100, skip } = parse_query(req.query)
+    // Limiting here because parse_query also used in export
+    const {
+        query,
+        sort,
+        order,
+        limit = 100,
+        skip,
+        select,
+    } = parse_query(req.query)
 
-  const items = await Image.find(query)
-    .sort({ [sort]: order })
-    .skip(Number(skip))
-    .limit(Math.max(Number(limit), 0))
+    const items = await Image.find(query)
+        .sort({ [sort]: order })
+        .skip(Number(skip))
+        .limit(Math.max(Number(limit), 0))
+        .select(select)
 
-  const total = await Image.countDocuments(query)
+    const total = await Image.countDocuments(query)
 
-  res.send({ total, skip, limit, items })
+    res.send({ total, skip, limit, items })
 }
 
 export const read_image = async (req: Request, res: Response) => {
-  const { _id } = req.params
-  const image = await Image.findOne({ _id })
-  if (!image) throw createHttpError(404, `Image ${_id} not found`)
-  res.send(image)
+    const { _id } = req.params
+    const image = await Image.findOne({ _id })
+    if (!image) throw createHttpError(404, `Image ${_id} not found`)
+    res.send(image)
 }
 
 export const read_image_file = async (req: Request, res: Response) => {
-  const { _id } = req.params
-  const image = await Image.findOne({ _id })
-  if (!image) throw createHttpError(404, `Image ${_id} not found`)
-  const { file } = image
-  const file_absolute_path = path.join(directories.uploads, file)
-  // Second argument is filename
-  res.download(file_absolute_path, file)
+    const { _id } = req.params
+    const image = await Image.findOne({ _id })
+    if (!image) throw createHttpError(404, `Image ${_id} not found`)
+    const { file } = image
+    const file_absolute_path = path.join(directories.uploads, file)
+    // Second argument is filename
+    res.download(file_absolute_path, file)
 }
 
 export const update_image = async (req: Request, res: Response) => {
-  const { _id } = req.params
-  const properties = req.body
+    const { _id } = req.params
+    const properties = req.body
 
-  const image = await Image.findOne({ _id })
-  if (!image) throw createHttpError(404, `Image ${_id} not found`)
+    const image = await Image.findOne({ _id })
+    if (!image) throw createHttpError(404, `Image ${_id} not found`)
 
-  // Unpack properties into data
-  // WARNING: Nested fields are replaced
-  image.data = { ...image.data, ...properties }
+    // Unpack properties into data
+    // WARNING: Nested fields are replaced
+    image.data = { ...image.data, ...properties }
 
-  // Deleting properties by setting them to null
-  // WARNING: this makes it imposible to have null properties
-  Object.keys(properties).forEach((k) => {
-    if (properties[k] === null) delete image.data[k]
-  })
+    // Deleting properties by setting them to null
+    // WARNING: this makes it imposible to have null properties
+    Object.keys(properties).forEach((k) => {
+        if (properties[k] === null) delete image.data[k]
+    })
 
-  const updated_image = await image.save()
-  res.send(updated_image)
+    const updated_image = await image.save()
+    res.send(updated_image)
 }
 
 export const replace_image_data = async (req: Request, res: Response) => {
-  const { _id } = req.params
-  const properties = req.body
+    const { _id } = req.params
+    const properties = req.body
 
-  const image = await Image.findOne({ _id })
-  if (!image) throw createHttpError(404, `Image ${_id} not found`)
+    const image = await Image.findOne({ _id })
+    if (!image) throw createHttpError(404, `Image ${_id} not found`)
 
-  // replace all properties
-  image.data = properties
-  const updated_image = await image.save()
-  res.send(updated_image)
+    // replace all properties
+    image.data = properties
+    const updated_image = await image.save()
+    res.send(updated_image)
 }
 
 export const delete_images = async (req: Request, res: Response) => {
-  const { query, sort, order, limit = 0, skip } = parse_query(req.query)
+    const { query, sort, order, limit = 0, skip } = parse_query(req.query)
 
-  const items = await Image.find(query)
-    .sort({ [sort]: order })
-    .skip(Number(skip))
-    .limit(Math.max(Number(limit), 0))
+    const items = await Image.find(query)
+        .sort({ [sort]: order })
+        .skip(Number(skip))
+        .limit(Math.max(Number(limit), 0))
 
-  // Delete files
-  const fileDeletePromises = items.map(({ file }) =>
-    remove_file(path.join(directories.uploads, file))
-  )
-  await Promise.all(fileDeletePromises)
+    // Delete files
+    const fileDeletePromises = items.map(({ file }) =>
+        remove_file(path.join(directories.uploads, file))
+    )
+    await Promise.all(fileDeletePromises)
 
-  // delete records
-  const recordDeletePromises = items.map(({ _id }) => Image.deleteOne({ _id }))
-  await Promise.all(recordDeletePromises)
+    // delete records
+    const recordDeletePromises = items.map(({ _id }) =>
+        Image.deleteOne({ _id })
+    )
+    await Promise.all(recordDeletePromises)
 
-  console.log(`${items.length} images deleted`)
-  res.send({ item_count: items.length })
+    console.log(`${items.length} images deleted`)
+    res.send({ item_count: items.length })
 }
 
 export const delete_image = async (req: Request, res: Response) => {
-  const { _id } = req.params
-  const image = await Image.findOneAndDelete({ _id })
-  if (!image) throw createHttpError(404, `Image ${_id} not found`)
-  const file_absolute_path = path.join(directories.uploads, image.file)
-  await remove_file(file_absolute_path)
-  res.send({ _id })
+    const { _id } = req.params
+    const image = await Image.findOneAndDelete({ _id })
+    if (!image) throw createHttpError(404, `Image ${_id} not found`)
+    const file_absolute_path = path.join(directories.uploads, image.file)
+    await remove_file(file_absolute_path)
+    res.send({ _id })
 }
--- utils.ts
+++ utils.ts
@@ -1,85 +1,86 @@
 // @ts-ignore
-import rimraf from "rimraf"
-import fs from "fs"
+import rimraf from 'rimraf'
+import fs from 'fs'
 
 export const remove_file = (file_path: string) =>
-  new Promise((resolve, reject) => {
-    rimraf(file_path, (error: any) => {
-      if (error) return reject(error)
-      resolve(null)
+    new Promise((resolve, reject) => {
+        rimraf(file_path, (error: any) => {
+            if (error) return reject(error)
+            resolve(null)
+        })
     })
-  })
 
 export const create_directory_if_not_exists = (target: string) => {
-  let stat = null
+    let stat = null
 
-  try {
-    stat = fs.statSync(target)
-  } catch (err) {
-    fs.mkdirSync(target, { recursive: true })
-  }
+    try {
+        stat = fs.statSync(target)
+    } catch (err) {
+        fs.mkdirSync(target, { recursive: true })
+    }
 
-  if (stat && !stat.isDirectory()) {
-    throw new Error(
-      `Directory cannot be created because an inode of a different type exists at ${target}`
-    )
-  }
+    if (stat && !stat.isDirectory()) {
+        throw new Error(
+            `Directory cannot be created because an inode of a different type exists at ${target}`
+        )
+    }
 }
 
 export const parse_query = (rawQuery: any) => {
-  const {
-    skip = 0,
-    limit = 10000,
-    sort = "time",
-    order = 1,
-    from,
-    to,
-    regex = false, // boolean toggling partial text search, not ideal
-    file,
-    filter,
-    ...rest
-  } = rawQuery
+    const {
+        skip = 0,
+        limit = 10000,
+        sort = 'time',
+        order = 1,
+        from,
+        to,
+        regex = false, // boolean toggling partial text search, not ideal
+        file,
+        filter,
+        select,
+        ...rest
+    } = rawQuery
 
-  // NOTE: partial text search on any field might not work because field list not fixed
+    // NOTE: partial text search on any field might not work because field list not fixed
 
-  let query: any = {}
+    let query: any = {}
 
-  if (file) {
-    if (regex) query.file = { $regex: file, $options: "i" }
-    else query.file = file
-  }
-
-  if (filter) {
-    try {
-      query = { ...query, ...JSON.parse(filter) }
-    } catch (error) {
-      throw "Malformed filter"
+    if (file) {
+        if (regex) query.file = { $regex: file, $options: 'i' }
+        else query.file = file
     }
-  }
 
-  for (const key in rest) {
-    let value = rest[key]
+    if (filter) {
+        try {
+            query = { ...query, ...JSON.parse(filter) }
+        } catch (error) {
+            throw 'Malformed filter'
+        }
+    }
 
-    // Convert numbers into numbers
-    // try {
-    //   value = JSON.parse(value)
-    // } catch (error) { }
+    for (const key in rest) {
+        let value = rest[key]
 
-    if (regex) query[`data.${key}`] = { $regex: value, $options: "i" }
-    else query[`data.${key}`] = value
-  }
+        // Convert numbers into numbers
+        // try {
+        //   value = JSON.parse(value)
+        // } catch (error) { }
 
-  // Time filters
-  // Using $gt and $lt instead of $gte and $lte for annotation tool
-  if (to || from) query.time = {}
-  if (to) query.time.$lt = new Date(to)
-  if (from) query.time.$gt = new Date(from)
+        if (regex) query[`data.${key}`] = { $regex: value, $options: 'i' }
+        else query[`data.${key}`] = value
+    }
 
-  return { query, to, from, limit, skip, sort, order }
+    // Time filters
+    // Using $gt and $lt instead of $gte and $lte for annotation tool
+    if (to || from) query.time = {}
+    if (to) query.time.$lt = new Date(to)
+    if (from) query.time.$gt = new Date(from)
+
+    return { query, to, from, limit, skip, sort, order, select }
 }
 
 export const parse_formdata_fields = (body: { json?: any; data?: any }) => {
-  const json_data = body.data || body.json
-  const data = json_data ? JSON.parse(json_data) : body
-  return data
+    const json_data = body.data || body.json
+    const data = json_data ? JSON.parse(json_data) : body
+    return data
 }
