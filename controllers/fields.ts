b673154c50949b22e431a8966e776e675649d9fc
Maxime Moreillon <m.moreillon@gmail.com>
2023-11-15 09:44:05 +0900

    dealing with missing fields

--- controllers/fields.ts
+++ controllers/fields.ts
@@ -1,23 +1,26 @@
-import Image from "../models/image"
-import { parse_query } from "../utils"
-import { Request, Response } from "express"
+import Image from '../models/image'
+import { parse_query } from '../utils'
+import { Request, Response } from 'express'
 
 export const read_fields = async (req: Request, res: Response) => {
-  const [{ fields }] = await Image.aggregate([
-    { $sample: { size: 100000 } },
-    { $project: { arrayofkeyvalue: { $objectToArray: "$$ROOT.data" } } },
-    { $unwind: "$arrayofkeyvalue" },
-    { $group: { _id: null, fields: { $addToSet: "$arrayofkeyvalue.k" } } },
-  ])
+    const [result] = await Image.aggregate([
+        { $sample: { size: 100000 } },
+        { $project: { arrayofkeyvalue: { $objectToArray: '$$ROOT.data' } } },
+        { $unwind: '$arrayofkeyvalue' },
+        { $group: { _id: null, fields: { $addToSet: '$arrayofkeyvalue.k' } } },
+    ])
 
-  res.send(fields)
+    if (!result) return res.send([])
+    const { fields } = result
+
+    res.send(fields)
 }
 
 export const read_field_unique_values = async (req: Request, res: Response) => {
-  const { field_name } = req.params
-  const { query } = parse_query(req.query)
+    const { field_name } = req.params
+    const { query } = parse_query(req.query)
 
-  const items = await Image.distinct(`data.${field_name}`, query)
+    const items = await Image.distinct(`data.${field_name}`, query)
 
-  res.send(items)
+    res.send(items)
 }
